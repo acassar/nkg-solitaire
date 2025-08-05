d7d7b6c501bf695ef0d34de684efbaaf2b5565ae
Adrien Cassar <adricass@live.fr>
2025-08-05 22:11:38 +0200

    empty card on empty tableau and prepared drop on it

--- src/components/TableauPile.vue
+++ src/components/TableauPile.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
+import { useCardDrag } from '@/services/useCardDrag'
 import Card from './CardComponent.vue'
 import type { Card as CardType } from '@/models/Card'
+import { useGameStateStore } from '@/stores/gameStateStore'
+import { storeToRefs } from 'pinia'
+
+const { gameState } = storeToRefs(useGameStateStore())
+const { drop } = useCardDrag(gameState)
 
 defineProps<{
   cards: CardType[]
@@ -9,7 +15,13 @@ defineProps<{
 
 <template>
   <div class="pile">
-    <div class="card-container" v-for="card in cards" :key="card.id">
+    <div
+      @dragover.prevent
+      @drop="(e) => drop(e)"
+      v-if="cards.length === 0"
+      class="card empty"
+    ></div>
+    <div v-else class="card-container" v-for="card in cards" :key="card.id">
       <Card :card />
     </div>
   </div>
